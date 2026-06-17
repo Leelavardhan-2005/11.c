c07456e1ccd8b04cce1bf7cfe379b227c3c68a49
Leelavardhan <vardhan@LAPTOP-8OTA07NN.localdomain>
2026-06-17 11:09:51 +0000

    HELLOW

---
+++ menu.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    char op;
+    printf("--------welcome to my Dhaba--------->\n\n");
+    printf("select what you want->\n");
+    printf("1.stater\n2.main course\n3.exit\n");
+
+    scanf("%c",&op);
+
+    switch(op)
+    {
+        case'1':
+        printf("you have choosed staters\n ");
+        printf("1.Dc\n2.pp\n3.CT\n4.Exit\n");
+        case'2':
+        printf("you have choosed staters\n ");
+        printf("1.\n2.\n3.\n4. Exit\n");
+        case'3':
+        printf("you have choosed staters\n ");
+        printf("1.\n2.\n3.\n4.Exit\n");
+        case'4':
+        printf("you have choosed staters\n ");
+        printf("1.Dc\n2.pp\n3.CT\n4.Exit\n");
+
+    }
+}
