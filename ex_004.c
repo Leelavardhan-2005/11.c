4e76768ae717e3649e5ea3c2da24d6585fb913a4
Leelavardhan-2005 <vardhanchidagondla@gmail.com>
2026-06-18 05:38:19 +0000

    third commit

---
+++ ex_004.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+     int num;
+     char cnum;
+     float fnum;
+     double dnum;
+
+     scanf("%d", &num);
+     printf("%d\n", num);
+     scanf("%c", &cnum);
+     printf("%c\n", cnum);
+     scanf("%f", &fnum);
+     printf("%f\n", fnum);
+     scanf("%lf", &dnum);
+     printf("%lf\n", dnum);
+
+     return 0;
+}
+
